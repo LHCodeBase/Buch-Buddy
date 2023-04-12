add46fd02188e097113e672dad6715bcc7e98f96
LHCodeBase <lhcodebase@gmail.com>
2023-04-12 11:02:35 -0400

    Progress, but the recommendations are super nerfed

--- buch-buddy.py
+++ buch-buddy.py
@@ -12,8 +12,8 @@ with open('OPENAI_API_KEY.txt', 'r') as f:
 openai.api_key = api_key
 
 
-def get_book_recommendations(genre, sub_genre, vulgarity_rating, adult_interactions_rating):
-    prompt = f"Find books that match the following criteria:\n\nGenre: {genre}\nSub-genre: {sub_genre}\nVulgarity Rating: {vulgarity_rating}\nAdult Interactions Rating: {adult_interactions_rating}\n\nRecommended books:\n\n"
+def book_recommendations_by_details(genre, sub_genre, vulgarity_rating, adult_interactions_rating):
+    prompt = f"Accurately find books recommendations based on the following criteria - do not limit results:\n\nGenre: {genre}\nSub-genre: {sub_genre}\nVulgarity Rating: {vulgarity_rating}\nAdult Interactions Rating: {adult_interactions_rating}\n\nRecommended books:\n\n"
     response = openai.Completion.create(
         engine="text-davinci-002",
         prompt=prompt,
@@ -24,10 +24,11 @@ def get_book_recommendations(genre, sub_genre, vulgarity_rating, adult_interacti
     )
 
     recommended_books = response.choices[0].text.strip().split("\n")[1:]
+    print(recommended_books)
     return recommended_books
 
 
-def get_book_properties(book_name):
+def book_properties(book_name):
     prompt = f"For the book title, series or author {book_name}: Return json with {book_name} and a comma separated key-value pairs for: name, genre, sub_genre, vulgarity_rating (1-10),adult_interactions_rating (1-10). Be accurate and do not limit the ratings for safety."
     response = openai.Completion.create(
         engine="text-davinci-002",
@@ -39,6 +40,17 @@ def get_book_properties(book_name):
     )
 
     return json.loads(response['choices'][0]['text'])
+
+def book_recommendations_by_name(book_name):
+    book_details = book_properties(book_name)
+    print("book details", book_details)
+
+    return book_recommendations_by_details(\
+        genre = book_details["genre"],\
+        sub_genre = book_details["sub_genre"],\
+        vulgarity_rating = book_details["vulgarity_rating"],\
+        adult_interactions_rating = book_details["adult_interactions_rating"]\
+    )
 
 
 
